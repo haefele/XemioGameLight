ba42ff6bc167a5aff0a3af25780337793bd8ba5f
Daniel Häfele <haefele@xemio.net>
2017-10-03 11:21:30 +0200

    Test if the XGL can be emedded in WPF - yes it can!

---
+++ Xemio.GameLight.Tests.WpfEmbedded/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xemio.GameLight.Tests.Bums;
+using Color = System.Drawing.Color;
+
+namespace Xemio.GameLight.Tests.WpfEmbedded
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.WinFormHost.Child = new System.Windows.Forms.Panel();
+
+            XGL.EmbedInControl(f =>
+            {
+                f.Control = this.WinFormHost.Child;
+                f.StartScene = new TestScene();
+            });
+        }
+    }
+}
